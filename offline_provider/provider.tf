14c2a93c598f44cc284b0f9340c6b20167a0291a
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-02 09:18:51 +0530

    gitignore added. offline provider - source keyword

---
+++ offline_provider/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+        required_providers {
+                google = {
+                        source = "hashicorp/google"
+                        version = "4.51.0"
+                }
+        }
+}
+provider "google" {
+        project = "cool-discipline-186213"
+        region = "us-weest1"
+        zone = "us-west1-b"
+        credentials = file("/Users/saurabhnair/Documents/personal/projects/terraform/training/terraform-cool-discipline-186213-884c8d890250.json")
+}
+resource "google_compute_network" "vpc_network" {
+        name = "terraform-network"
+}
