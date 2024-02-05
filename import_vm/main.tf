f6d37b522f98c8aa24f9377246ad5505d02605cf
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-05 13:07:18 +0530

    Importing VM. Creating Workspaces

---
+++ import_vm/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_instance" "default" {
+	name = "terraform-dev-vm-01"
+	machine_type = "e2-micro"
+	zone = "us-central1-a"
+
+	boot_disk {
+	initialize_params {
+		image                       = "debian-12-bookworm-v20240110"
+		}
+	}
+	network_interface {
+		access_config{
+
+		}
+	}
+}
+
+
+
+
+# You can have just resource 	`"google_compute_instance" "default" {}` in your main.tf
+# `terraform-dev-vm-01` is the name of the instance on GCP which is manually created. Get state file from GCP using the below command:
+# terraform import google_compute_instance.default terraform-dev-vm-01
+# Now you will have state file updated but terraform code won't be there.
+# So manually add certain important keys and values to main.tf
+
---
+++ workspaces/main.tf
@@ -0,0 +1,27 @@
+resource "google_compute_instance" "default" {
+	name = "terraform-dev-vm-02"
+	machine_type = "e2-small"
+	zone = "us-west1-a"
+
+	boot_disk {
+	initialize_params {
+		image                       = "debian-12-bookworm-v20240110"
+		}
+	}
+	network_interface {
+		network="default"
+		access_config{
+
+		}
+	}
+}
+
+# Workspace acts like branches of git. Change certain properties to create resources with the same code.
+# terraform workspace show
+# terraform workspace new new_space_01
+# Change either your provider.tf / main.tf
+# terraform plan
+# terraform apply
+# terraform workspace select default
+# terraform plan
+# terraform apply
