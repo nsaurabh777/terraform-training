128d8ac4d0adfba6fb6b28e24c2f15b765f5f463
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-05 13:08:36 +0530

    formatted

--- import_vm/main.tf
+++ import_vm/main.tf
@@ -1,18 +1,18 @@
 resource "google_compute_instance" "default" {
-	name = "terraform-dev-vm-01"
-	machine_type = "e2-micro"
-	zone = "us-central1-a"
+  name         = "terraform-dev-vm-01"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
 
-	boot_disk {
-	initialize_params {
-		image                       = "debian-12-bookworm-v20240110"
-		}
-	}
-	network_interface {
-		access_config{
+  boot_disk {
+    initialize_params {
+      image = "debian-12-bookworm-v20240110"
+    }
+  }
+  network_interface {
+    access_config {
 
-		}
-	}
+    }
+  }
 }
 
 
--- workspaces/main.tf
+++ workspaces/main.tf
@@ -1,19 +1,19 @@
 resource "google_compute_instance" "default" {
-	name = "terraform-dev-vm-02"
-	machine_type = "e2-small"
-	zone = "us-west1-a"
+  name         = "terraform-dev-vm-02"
+  machine_type = "e2-small"
+  zone         = "us-west1-a"
 
-	boot_disk {
-	initialize_params {
-		image                       = "debian-12-bookworm-v20240110"
-		}
-	}
-	network_interface {
-		network="default"
-		access_config{
+  boot_disk {
+    initialize_params {
+      image = "debian-12-bookworm-v20240110"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
 
-		}
-	}
+    }
+  }
 }
 
 # Workspace acts like branches of git. Change certain properties to create resources with the same code.
