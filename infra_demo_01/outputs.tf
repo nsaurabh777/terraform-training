dde57e52d8fed505d003765313caa4bb15e317cb
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-08 14:24:25 +0530

    SQL DB uncommented

--- infra_demo_01/outputs.tf
+++ infra_demo_01/outputs.tf
@@ -6,6 +6,6 @@ output "backend_ip" {
   value = google_compute_instance_group_manager.backend_instance_group.instance_group
 }
 
-# output "database_ip" {
-#   value = google_sql_database_instance.db_instance.ip_address
-# }
+output "database_ip" {
+  value = google_sql_database_instance.db_instance.ip_address
+}
