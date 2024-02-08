982aa92e64a8da3a372d56fedc736fee2ee46889
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-08 09:52:10 +0530

    formatting

--- module_example/main.tf
+++ module_example/main.tf
@@ -1,13 +1,13 @@
 module "my_modules" {
-	source = "./modules"
-	bucket_name = "my-demo-cloud-bkt-777"
-	instance_name = "my-module-instance"
-	instance_type = "e2-micro"
-	network_interface = {
-		network = google_compute_network.vpc_network.self_link
-	}
+  source        = "./modules"
+  bucket_name   = "my-demo-cloud-bkt-777"
+  instance_name = "my-module-instance"
+  instance_type = "e2-micro"
+  network_interface = {
+    network = google_compute_network.vpc_network.self_link
+  }
 }
 
 resource "google_compute_network" "vpc_network" {
-	name = "my-module-network"
+  name = "my-module-network"
 }
--- module_example/modules/cloud_bucket.tf
+++ module_example/modules/cloud_bucket.tf
@@ -1,8 +1,8 @@
 variable "bucket_name" {
-	description = "name of the cloud storage"
+  description = "name of the cloud storage"
 }
 
 resource "google_storage_bucket" "my_bucket" {
-	name = var.bucket_name
-	location = "us"
+  name     = var.bucket_name
+  location = "us"
 }
