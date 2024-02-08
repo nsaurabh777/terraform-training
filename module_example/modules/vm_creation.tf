63ca55efa2cc255113b59543792b37fff8ce83bf
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-08 09:19:35 +0530

    vm_creation using modules added

---
+++ module_example/modules/vm_creation.tf
@@ -0,0 +1,20 @@
+variable "instance_name" {}
+variable "instance_type" {}
+variable "network_interface" {}
+
+resource "google_compute_instance" "vm_instance" {
+	name = var.instance_name
+	machine_type = var.instance_type
+	zone = "us-central1-a"
+	boot_disk {
+		initialize_params {
+			image = "debian-cloud/debian-10"
+		}
+	}
+	network_interface {
+		network = var.network_interface["network"]
+		access_config {
+		
+		}
+	}
+}
