0f7306214f8de49f5160621eabcb9a00b4aa0cb7
Saurabh Nair <saurabhnair@Saurabhs-MacBook-Pro.local>
2024-02-08 12:50:30 +0530

    boolean if clause. validation clause

---
+++ variables_example/bool_example.tf
@@ -0,0 +1,20 @@
+variable "create_instance" {
+  type = bool
+  # default = true
+}
+
+resource "google_compute_instance" "bool_instance" {
+  count = var.create_instance ? 1 : 0
+  name = "my-bool-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
---
+++ variables_example/validation.tf
@@ -0,0 +1,13 @@
+variable "env" {
+  type = string
+  description = "production / staging / development"
+  validation {
+    condition = can(regex("^(prod|stag|dev)$", var.env))
+    error_message = "invalid env"
+  }
+}
+
+resource "google_storage_bucket" "validation_example" {
+  name = "my-validation-bkt-${var.env}"
+  location = "us-central1"
+}
